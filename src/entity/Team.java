a09d3ff8d9d555d95d7b665215dd15a8f74260f1
Sven Lindqvist <Sven-Lindqvist@hotmail.com>
2018-03-26 12:38:32 +0200

    Hurra

--- src/entity/Team.java
+++ src/entity/Team.java
@@ -10,73 +10,78 @@ public class Team {
 	private int[] tablePosition = new int[38];
 	private String name;
 	private int id;
-	
-	public Team (String name) {
-		this.name = name;
+
+	public Team(String name) {
+		this.name = name; //
 	}
-	
+
 	public void setGoalsFor(int round, int value) {
-		goalsFor[round-1] = value;
+		goalsFor[round - 1] = value;
 	}
-	
+
 	public int getGoalsFor(int round) {
-		return goalsFor[round-1];
+		return goalsFor[round - 1];
 	}
-	
+
 	public void setGoalsAgainst(int round, int value) {
-		goalsAgainst[round-1] = value;
+		goalsAgainst[round - 1] = value;
 	}
-	
+
 	public int getGoalsAgainst(int round) {
-		return goalsAgainst[round-1];
+		return goalsAgainst[round - 1];
 	}
-	
+
 	public void setPoints(int round, int value) {
-		points[round-1] = value;
+		points[round - 1] = value;
 	}
-	
+
 	public int getPoints(int round) {
-		return points[round-1];
+		return points[round - 1];
 	}
-	
+
 	public void setPointsHome(int round, int value) {
-		pointsHome[round-1] = value;
+		pointsHome[round - 1] = value;
 	}
-	
+
 	public int getPointsHome(int round) {
-		return pointsHome[round-1];
+		return pointsHome[round - 1];
 	}
-	
+
 	public void setPointsAway(int round, int value) {
-		pointsAway[round-1] = value;
+		pointsAway[round - 1] = value;
 	}
-	
+
 	public void setTablePosition(int round, int value) {
-		tablePosition[round-1] = value;
+		tablePosition[round - 1] = value;
 	}
-	
+
 	public int getTablePosition(int round) {
-		return tablePosition[round-1];
+		return tablePosition[round - 1];
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int pointsLastNGames(int round, int number) {
-		
-		return points[round] - points[round-number];
+
+		return points[round] - points[round - number];
 	}
+
 	public String toString() {
 		return this.name;
 	}
-	
+
 }

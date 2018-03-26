272585cfb4f01a9ff391467bcafddda68e55a9dd
Oskar Engström Magnusson <oggeem95@live.se>
2018-03-26 16:09:47 +0200

    skapat FixtureHandler

--- src/control/FixtureHandler.java
+++ src/control/FixtureHandler.java
@@ -1,7 +1,48 @@
 package control;
 
-public class FixtureHandler {
+import org.json.*;
+import entity.*;
 
-	
+public class FixtureHandler {
+	private static JSONObject jsonMatches;
+	private static JSONArray fixtures;
+
+	public static void createFixtures(Season season) throws JSONException {
+		Match match;
+		int homeGoals;
+		int awayGoals;
+		int matchDay;
+		jsonMatches = FetchApi.getJsonMatches(season.getId());
+		fixtures = jsonMatches.getJSONArray("fixtures");
+		
+		for(int i = 0; i<fixtures.length(); i++) {
+			String status = fixtures.getJSONObject(i).getString("status");
+			if (status.equals("FINISHED")) {
+				
+				Team homeTeam = season.getTeam(fixtures.getJSONObject(i).getString("homeTeamName"));
+				Team awayTeam = season.getTeam(fixtures.getJSONObject(i).getString("awayTeamName"));
+				homeGoals = fixtures.getJSONObject(i).getJSONObject("result").getInt("goalsHomeTeam");
+				awayGoals = fixtures.getJSONObject(i).getJSONObject("result").getInt("goalsAwayTeam");
+				matchDay = fixtures.getJSONObject(i).getInt("matchday");
+				
+				match = new Match(homeTeam, awayTeam);
+				
+				match.setIsFinished(status);
+				match.setHomeGoals(homeGoals);
+				match.setAwayGoals(awayGoals);
+				match.setRound(matchDay);
+				homeTeam.setGoalsFor(matchDay, homeGoals);
+				homeTeam.setGoalsAgainst(matchDay, awayGoals);
+				awayTeam.setGoalsFor(matchDay, awayGoals);
+				awayTeam.setGoalsAgainst(matchDay, homeGoals);
+				match.setOutcome();
+				
+				season.addMatch(match);
+			}	
+		}
+	}
+
+
+
 	
 }

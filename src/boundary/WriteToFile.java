46fd49efd9d093ff30ec40fba13ad51834e9a052
Vixen666 <lightqueer@hotmail.com>
2018-05-16 13:33:17 +0200

    write to file moved file

--- src/boundary/WriteToFile.java
+++ src/boundary/WriteToFile.java
@@ -42,7 +42,7 @@ public class WriteToFile {
 	 * @param txt the text to write
 	 */
 	public static void appendTxt(String txt) {
-		try (FileWriter fw = new FileWriter("testMatcher.txt", true);
+		try (FileWriter fw = new FileWriter("/SavedFiles/testMatcher.txt", true);
 				BufferedWriter bw = new BufferedWriter(fw);
 				PrintWriter out = new PrintWriter(bw)) {
 			out.println(txt);
